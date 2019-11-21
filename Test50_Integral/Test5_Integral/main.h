fa081685bcd1ff79377b4311c37805d869e69d00
wushulu <1084116849@qq.com>
2019-11-21 21:50:15 +0800

    test50

---
+++ Test50_Integral/Test5_Integral/main.h
@@ -0,0 +1,30 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#endif
+
+void Send_zero();
+void Send_one();
+void Send_two();
+void Send_three();
+void Send_fuor();
+
+void Send_zero()
+{
+ std ::cout << "zero\r" << std ::endl;
+}
+void Send_one()
+{
+ std ::cout << "one\r" << std ::endl;
+}
+void Send_two()
+{
+ std ::cout << "two\r" << std ::endl;
+}
+void Send_three()
+{
+ std ::cout << "three\r" << std ::endl;
+}
+void Send_fuor()
+{
+ std ::cout << "fuor\r" << std ::endl;
+}
